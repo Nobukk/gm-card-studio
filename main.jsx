7d6fd86638c141163cfddc6729f9023f18101f55
Nobukk <162643895+Nobukk@users.noreply.github.com>
2026-03-24 00:36:42 +0100

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GMCardGenerator from './gm-card-generator'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GMCardGenerator />
+  </React.StrictMode>
+)
